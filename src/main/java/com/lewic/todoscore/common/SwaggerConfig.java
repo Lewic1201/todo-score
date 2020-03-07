14fc30ac2a1314d808a1e565863833e5e6e8be42
Lewic <lpf1201@qq.com>
2020-03-07 22:43:09 +0800

    swagger支持token

--- src/main/java/com/lewic/todoscore/common/SwaggerConfig.java
+++ src/main/java/com/lewic/todoscore/common/SwaggerConfig.java
@@ -3,23 +3,41 @@ package com.lewic.todoscore.common;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@EnableSwagger2
 @Configuration
-public class Swagger2 {
+public class SwaggerConfig {
 
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder tokenParam = new ParameterBuilder();
+        List<Parameter> params = new ArrayList<>();
+        //header中的X-TOKEN参数非必填，传空也可以
+        tokenParam.name("X-TOKEN").description("token")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build();
+        //添加公共参数
+        params.add(tokenParam.build());
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
                 .select()
+//                .apis(RequestHandlerSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.lewic.todoscore"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(params)
+                .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
